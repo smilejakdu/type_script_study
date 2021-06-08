5dad6478a56669be856025998e51fffd16c090a8
smilejakdu <ash982416@gmail.com>
2021-06-09 00:14:26 +0900

    frist commit

---
+++ src/1.typescript_start/1.ts
@@ -0,0 +1,11 @@
+
+
+import { f1 } from './2';
+console.log(1 + 2);
+// control + option + n 
+f1
+
+// Auto import : command + .
+// 멀티 선택 : 원하는 단어 선택 후, command + d
+// 선택 취소할 때는 esc
+// 멀티 커서 : command + option + 화살표
