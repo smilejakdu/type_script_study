beadbdcc2a7bc3573e86facef05bf9eba77f6318
smilejakdu <ash982416@gmail.com>
2021-08-22 01:50:30 +0900

    generic 복습

--- inflean_lee/src/10.generic/6.ts
+++ inflean_lee/src/10.generic/6.ts
@@ -1,37 +1,46 @@
 import { size } from "lodash";
 
-export { };
+export {};
 
 interface Person {
-	name: string;
-	age: number;
+  name: string;
+  age: number;
 }
 
 interface Korean extends Person {
-	liveInSeoul : boolean;
+  liveInSeoul: boolean;
 }
 
 // type T1 = keyof Person;
-function swapProperty<T extends Person, K extends keyof Person > (
-	p1: T,
-	p2: T,
-	key: K,
+function swapProperty<T extends Person, K extends keyof Person>(
+  // K extends keyof Person => type이 name 이나 age 가 된다.
+  p1: T,
+  p2: T,
+  key: K
 ): void {
-	const temp = p1[key];
-	p1[key] = p2[key];
-	p2[key] = temp;
+  const temp = p1[key];
+  p1[key] = p2[key];
+  p2[key] = temp;
 }
 
-const p1 : Korean = {
-	name: '홍길동',
-	age: 23,
-	liveInSeoul: true,
+const p1: Korean = {
+  name: "홍길동",
+  age: 23,
+  liveInSeoul: true,
 };
 
 const p2: Korean = {
-	name: '김삿갓',
-	age: 31,
-	liveInSeoul: false,
+  name: "김삿갓",
+  age: 31,
+  liveInSeoul: false,
 };
 
-swapProperty(p1, p2, 'age');
+swapProperty(p1, p2, "age");
+swapProperty(p1, p2, true); // error
+/*
+에러가 발생하는 이유는 K 타입엔 string 또는 number 가 가능하기 때문이다.
+interface Person {
+  name: string;
+  age: number;
+}
+*/
--- inflean_lee/src/10.generic/7.ts
+++ inflean_lee/src/10.generic/7.ts
@@ -1,13 +1,34 @@
-function list_merge1<A>(a:Array<A>, b:Array<A>): Array<A> {
-	return a.concat(b);
+export {};
+// type T1 = keyof Person;
+interface Person {
+  name: string;
+  age: number;
 }
 
-function list_merge2<A extends string[], B extends string[]>(a: A, b: B): string[] {
-	return a.concat(b);
+function swapProperty<T extends Person, K extends keyof Person>(
+  // Person 의 키니깐 name 과 age 이다.
+  p1: T,
+  p2: T,
+  key: K
+): void {
+  const temp = p1[key];
+  p1[key] = p2[key];
+  p2[key] = temp;
+}
+interface Product {
+  name: string;
+  price: number;
 }
 
-const merged3 = list_merge1(["1"], ["string1", "string2", "string3"])
-console.log(merged3); // ['1', 'string1', 'string2', 'string3' ]
+const p1: Product = {
+  name: "시계",
+  price: 1000,
+};
 
-const merged4 = list_merge2(["1"], ["string1", "string2", "string3"])
-console.log(merged4); // ['1', 'string1', 'string2', 'string3' ]
+const p2: Product = {
+  name: "자전거",
+  price: 2000,
+};
+
+swapProperty(p1, p2, "name");
+// p1 은 Product 타입이기 때문에 Person 에 할당 가능하지 않다.
