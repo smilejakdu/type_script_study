7a059bd01f5b684de8eb4b3832a793eadc5c8a83
smilejakdu <ash982416@gmail.com>
2021-10-07 09:49:24 +0900

    타입넓히기

--- effective_typescript/test.ts
+++ effective_typescript/test.ts
@@ -1,4 +1,18 @@
-const axis1: string = 'x';  // Type is string
-const test = 'z';
-const number = 2;
-const axis2 = 'y';  // Type is "y"
+interface Vector3 { x: number; y: number; z: number; }
+function getComponent(vector: Vector3, axis: 'x' | 'y' | 'z') {
+  return vector[axis];
+}
+const v1 = {
+  x: 1,
+  y: 2,
+};  // Type is { x: number; y: number; }
+
+const v2 = {
+  x: 1 as const,
+  y: 2,
+};  // Type is { x: 1; y: number; }
+
+const v3 = {
+  x: 1,
+  y: 2,
+} as const;  // Type is { readonly x: 1; readonly y: 2; }
