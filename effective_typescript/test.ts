8fe15a731f9d3314a959684bbfd4f16f824a7d35
smilejakdu <ash982416@gmail.com>
2021-10-07 10:04:51 +0900

    타입넓히기

--- effective_typescript/test.ts
+++ effective_typescript/test.ts
@@ -1,18 +1,2 @@
-interface Vector3 { x: number; y: number; z: number; }
-function getComponent(vector: Vector3, axis: 'x' | 'y' | 'z') {
-  return vector[axis];
-}
-const v1 = {
-  x: 1,
-  y: 2,
-};  // Type is { x: number; y: number; }
-
-const v2 = {
-  x: 1 as const,
-  y: 2,
-};  // Type is { x: 1; y: number; }
-
-const v3 = {
-  x: 1,
-  y: 2,
-} as const;  // Type is { readonly x: 1; readonly y: 2; }
+let test = 'string'; // 타입이 string
+const test2 = '제대로 되나'; // 타입이 "제대로 되나"
