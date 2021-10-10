3469b2862c746a5bfb50abb178fdef47dacf6cd9
smilejakdu <ash982416@gmail.com>
2021-10-10 17:07:41 +0900

    effective typescript lodash

--- effective_typescript/test.ts
+++ effective_typescript/test.ts
@@ -1,8 +1,19 @@
-declare let hasMiddle: boolean;
-const firstLast = {first: 'Harry', last: 'Truman'};
-const president = {...firstLast, ...(hasMiddle ? {middle: 'S'} : {})};
-president.middle
-       // ~~~~~~ Property 'middle' does not exist on type
-       //        '{ first: string; last: string; }'
-
-console.log(president);
+//프로미스 생성
+const promise1 = function (param) {
+  return new Promise(function (resolve, reject) {
+    if (param) {
+      resolve("바보");
+    } else {
+      reject("아닌데");
+    }
+  });
+};
+//프로미스 실행
+promise1(true).then(
+  function (result) {
+    console.log(result); //바보
+  },
+  function (err) {
+    console.log(err); //아닌데
+  }
+);
--- test.ts
+++ test.ts
@@ -1,10 +1,2 @@
-/*
-Input: nums = [0,2,1,5,3,4]
-Output: [0,1,2,4,5,3]
-Explanation: The array ans is built as follows: 
-ans = [nums[nums[0]], nums[nums[1]], nums[nums[2]], nums[nums[3]], nums[nums[4]], nums[nums[5]]]
-    = [nums[0], nums[2], nums[1], nums[5], nums[3], nums[4]]
-    = [0,1,2,4,5,3]
-*/
-
-function buildArray(nums: number[]): number[] {}
+const a1 = [1, 2, 3];          // 타입이 number []
+const a2 = [1, 2, 3] as const; // 타입이 readonly [1, 2, 3]
